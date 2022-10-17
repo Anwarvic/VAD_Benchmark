28c3065fb4cf8bebd82d380270e3fa6b6f9737e9
anwarvic <mohamedanwarvic@gmail.com>
2022-10-17 11:42:20 -0700

    changes the window_size hyper-parameter in Silero from frames to ms

--- vads/silero.py
+++ vads/silero.py
@@ -10,7 +10,7 @@ class Silero(Vad):
             threshold: float = 0.5,
             min_speech_duration_ms: int = 250,
             min_silence_duration_ms: int = 100,
-            window_size_samples: int = 1536,
+            window_size_ms: int = 96,
             speech_pad_ms: int = 30,
         ):
         self._vad, _ = torch.hub.load(
@@ -22,37 +22,38 @@ class Silero(Vad):
         self._threshold = threshold
         self._min_speech_duration_ms = min_speech_duration_ms
         self._min_silence_duration_ms = min_silence_duration_ms
-        self._window_size_samples = window_size_samples
+        self._window_size_ms = window_size_ms
         self._speech_pad_ms = speech_pad_ms
         self._valid_sr = [8000, 16000]
-        
-    
+
+
     def _preprocess_audio(self, audio, sr):
-        if sr == 8000 and self._window_size_samples > 768:
+        if sr == 8000 and self._window_size_ms > 48:
             warnings.warn(
-                "window_size_samples is too big for 8000 sampling_rate! " +
-                "Better set window_size_samples to 256, 512 or 768 for 8000" + 
+                "window_size_ms is too big for 8000 sampling_rate! " +
+                "Better set window_size_ms to 16, 32 or 48 for 8000" + 
                 " sample rate!"
             )
-        if self._window_size_samples not in [256, 512, 768, 1024, 1536]:
+        if self._window_size_ms not in [15, 32, 48, 64, 96]:
             warnings.warn(
-                "Unusual window_size_samples! Try:\n" +
-                "- [512, 1024, 1536] for 16000 sampling_rate\n" +
-                "- [256, 512, 768] for 8000 sampling_rate"
+                "Unusual window_size_ms! Try:\n" +
+                "- [32, 64, 96] for 16000 sampling_rate\n" +
+                "- [16, 32, 48] for 8000 sampling_rate"
             )
         # change audio to mono & fix sample rate
         audio, sr = super()._preprocess_audio(audio, sr)
         # convert audio to 1d array
         audio = audio.squeeze()
         return audio, sr
-    
+
 
     def _split_to_frames(self, audio, sr):
         self._vad.reset_states()
         end_sample = len(audio)
+        window_size_samples = sr * self._window_size_ms // 1000
 
-        for start_sample in range(0, end_sample, self._window_size_samples):
-            chunk = audio[start_sample:start_sample+self._window_size_samples]
+        for start_sample in range(0, end_sample, window_size_samples):
+            chunk = audio[start_sample : start_sample+window_size_samples]
             yield {
                 "data": chunk,
                 "start": start_sample, # in samples
@@ -67,15 +68,16 @@ class Silero(Vad):
         neg_threshold = self._threshold - 0.15
         temp_end = 0
 
-        min_speech_samples = sr * self._min_speech_duration_ms / 1000
-        min_silence_samples = sr * self._min_silence_duration_ms / 1000
+        min_speech_samples = sr * self._min_speech_duration_ms // 1000
+        min_silence_samples = sr * self._min_silence_duration_ms // 1000
+        window_size_samples = sr * self._window_size_ms // 1000
         audio_length_samples = len(audio)
         for i, frame in enumerate(frames):
             # pad frame if shorter than window_size
-            if len(frame["data"]) < self._window_size_samples:
+            if len(frame["data"]) < window_size_samples:
                 frame["data"] = torch.nn.functional.pad(
                     frame["data"],
-                    (0, int(self._window_size_samples - len(frame["data"])))
+                    (0, int(window_size_samples - len(frame["data"])))
                 )
             # check if frame has speech in it
             speech_prob = self._vad(frame["data"], sr).item()
@@ -84,13 +86,13 @@ class Silero(Vad):
 
             if (speech_prob >= self._threshold) and not triggered:
                 triggered = True
-                current_speech['start'] = self._window_size_samples * i
+                current_speech['start'] = window_size_samples * i
                 continue
 
             if (speech_prob < neg_threshold) and triggered:
                 if not temp_end:
-                    temp_end = self._window_size_samples * i
-                if (self._window_size_samples * i) - temp_end < min_silence_samples:
+                    temp_end = window_size_samples * i
+                if (window_size_samples * i) - temp_end < min_silence_samples:
                     continue
                 else:
                     current_speech['end'] = temp_end
@@ -105,7 +107,7 @@ class Silero(Vad):
             current_speech['end'] = audio_length_samples
             speech_frames.append(current_speech)
         
-        speech_pad_samples = sr * self._speech_pad_ms / 1000
+        speech_pad_samples = sr * self._speech_pad_ms // 1000
         for i, speech in enumerate(speech_frames):
             if i == 0:
                 speech['start'] = int(
@@ -143,7 +145,7 @@ class Silero(Vad):
                 )
             speech["data"] = audio[speech["start"]: speech["end"]]
         return speech_frames
-    
+
 
     def _merge_speech_frames(self, speech_frames):
         return torch.cat([
@@ -190,14 +192,15 @@ class Silero(Vad):
 if __name__ == "__main__":
     from os.path import dirname, abspath, join
     from utils import load_audio, save_audio
-    
+
     print("Running Silero Vad")
-    # samples_dir = join(dirname(dirname(abspath(__file__))), "samples")
-    # audio_filepath = join(samples_dir, "example_16k.wav")
-    audio_filepath = "dataset/ava_speech/_a9SWtcaNj8.wav"
+    samples_dir = join(dirname(dirname(abspath(__file__))), "samples")
+    audio_filepath = join(samples_dir, "example_16k.wav")
     audio, sr = load_audio(audio_filepath)
-    
+
     vad = Silero()
-    print(vad.get_speech_boundaries(audio, sr))
-    # audio, sr = vad.trim_silence(audio, sr)
+    # print(vad.get_speech_boundaries(audio, sr))
+    audio, sr = vad.trim_silence(audio, sr)
     # save_audio(audio, sr, join(samples_dir, "silero_example_16k.wav"))
+    save_audio(audio, sr, "silero_example_16k.wav")
+    
