dd352eaa1cd9000c4469605104ef553e7c3f21c5
anwarvic <mohamedanwarvic@gmail.com>
2022-10-25 13:33:12 -0700

    moved trim_silence() method to vad

--- vads/vad.py
+++ vads/vad.py
@@ -29,9 +29,34 @@ class Vad:
         return audio, sr
     
 
+    def _split_to_frames(self, audio, sr):
+        raise NotImplementedError
+    
+
+    def _get_speech_frames(self, frames, audio, sr):
+        raise NotImplementedError
+
+
     def get_speech_boundaries(self, audio, sr):
         raise NotImplementedError
+    
+
+    def _merge_speech_frames(self, speech_frames, audio, sr):
+        pass
 
 
     def trim_silence(self, audio, sr):
-        raise NotImplementedError
+        orig_sr = sr
+        # preprocess audio if needed
+        audio, sr = self._preprocess_audio(audio, sr)
+        # get frames having speech
+        frames = list(self._split_to_frames(audio, sr))
+        speech_frames = self._get_speech_frames(frames, audio, sr)
+        # merge speech frames
+        audio, sr = self._merge_speech_frames(speech_frames, audio, sr)
+        # post-process audio if needed
+        audio, sr = self._postprocess_audio(audio, sr)
+        # change sample rate back to the original one
+        if sr != orig_sr:
+            audio = change_sample_rate(audio, sr, orig_sr)
+        return audio, orig_sr
